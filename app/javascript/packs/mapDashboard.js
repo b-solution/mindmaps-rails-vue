c703592a718b4a89504104050523ee4273afc6a8
devawais <devawais.aslam@gmail.com>
2019-08-17 21:16:37 +0500

    added vue-routers for mindmaps new action and input fields for new idea node

--- app/javascript/packs/mapDashboard.js
+++ app/javascript/packs/mapDashboard.js
@@ -1,9 +1,10 @@
 import Mindmap from 'components/mindmaps/mindmap.vue';
+import router from 'routers/mindmaps'
 
 // eslint-disable-next-line no-unused-vars
 const mindmapApp = new Vue({
+  router,
   el: '#mapDashboard',
-  components: { Mindmap },
   template: '<Mindmap />',
-  replace: false
+  components: { Mindmap }
 });
