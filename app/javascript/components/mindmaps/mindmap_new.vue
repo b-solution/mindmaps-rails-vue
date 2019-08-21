b40ce9b12f6e1bb40f4ddbc35f704868fbdf2f82
devawais <devawais.aslam@gmail.com>
2019-08-21 16:41:33 +0500

    made new, save, open and reset buttons working

--- app/javascript/components/mindmaps/mindmap_new.vue
+++ app/javascript/components/mindmaps/mindmap_new.vue
@@ -1,30 +1,159 @@
 <template>
   <div>
     <section v-if="!loading" id="map-container" class="map-container" @mousemove.prevent="doDrag">
-      <div class="center">
+      <div class="center" :style="C_centeralNodePosition">
         <span @mousedown="startDrag" class="start_dot"></span>
         <input type="text" v-model="mainIdea" class="central_idea"/>
       </div>
       <input-field v-for="node in nodes" :key="`${node.left}.${node.top}`" v-model="node.value" @start-drag="startDrag" :style="getNodeStyle(node)" class="pos_abs"></input-field>
-      <canvas id="map-canvas" :width="windowWidth" height="1500"></canvas>
+      <canvas id="map-canvas" :width="windowWidth" :height="windowHeight"></canvas>
     </section>
     <div class="buttons_area">
       <span>
-        <button class="options_button">New</button>
-        <button class="options_button">Save</button>
-        <button class="options_button">Open</button>
-        <button class="options_button">Reset</button>
+        <button @click.stop="$refs.newMapModal.open" class="options_button">New</button>
+        <button @click.stop="$refs.saveMapModal.open" class="options_button">Save</button>
+        <button @click.stop="$refs.openMapModal.open" class="options_button">Open</button>
+        <button @click.stop="$refs.resetMapModal.open" class="options_button">Reset</button>
       </span>
     </div>
+    <sweet-modal ref="newMapModal" class="of_v">
+      <div class="sweet_model_icon_div">
+        <div class="radius_circle bg-warning center_flex mlr_a">
+          <i class="material-icons text-white">error_outline</i>
+        </div>
+      </div>
+      <h3 class="f_smooth_auto">Are you Sure?</h3>
+      <p class="text-muted fs_18">Your current changes will be discarded, you won't be able to revert this. You may want to open in new Tab!</p>
+      <div class="center_flex mt_2">
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-danger text-white mr_1"
+          @click.stop="openNewMap" 
+        >
+          <i class="material-icons mr-1">done</i>
+          Continue
+        </a>
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-warning text-white mr_1"
+          @click.stop="openNewMapNewWindow" 
+        >
+          <i class="material-icons mr-1">open_in_new</i>
+          Open in new Tab!
+        </a>
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-primary text-white mr_1"
+          @click.stop="$refs.newMapModal.close" 
+        >
+          <i class="material-icons mr-1">cancel</i>
+          Cancel
+        </a>
+      </div>
+    </sweet-modal>
+    <sweet-modal ref="saveMapModal" class="of_v">
+      <div class="sweet_model_icon_div">
+        <div class="radius_circle bg-success center_flex mlr_a">
+          <i class="material-icons text-white">save</i>
+        </div>
+      </div>
+      <div class="col form-inline center_flex">
+        <label for="mindmap_name">Mindmap Name:</label>
+        <input type="text" placeholder="Enter a unique name" v-model="saveMapName" name="mindmap[name]" class="ml-3 form-control">
+      </div>
+      <div class="center_flex mt_2">
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-success text-white mr_1"
+          @click.stop="saveCurrentMap" 
+        >
+          <i class="material-icons mr-1">done</i>
+          Save
+        </a>
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-primary text-white mr_1"
+          @click.stop="$refs.saveMapModal.close" 
+        >
+          <i class="material-icons mr-1">cancel</i>
+          Cancel
+        </a>
+      </div>
+    </sweet-modal>
+    <sweet-modal ref="openMapModal" class="of_v">
+      <div class="sweet_model_icon_div">
+        <div class="radius_circle bg-success center_flex mlr_a">
+          <i class="material-icons text-white">folder_open</i>
+        </div>
+      </div>
+      <p class="text-muted fs_18">Just a heads-up! Your current map will be discarded. You may want save this before opening a new map.</p>
+      <p class="text-muted fs_18">Enter the map details you want to open here!</p>
+      <form class="form-horizontal">   
+        <div class="row form-group">
+          <label class="control-label col-4" for="mindmap_name">Mindmap Name:</label>
+          <input type="text" placeholder="Enter map name" v-model="openMapmind.name" name="mindmap[name]" class="form-control col-6">
+        </div>
+        <div class="row form-group mt-2">
+          <label class="control-label col-4" for="mindmap_name">Mindmap id:</label>
+          <input type="text" placeholder="Enter map id" v-model="openMapmind.id" name="mindmap[name]" class="form-control col-6 ">
+        </div>
+      </form>
+      <div class="center_flex mt_2">
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-success text-white mr_1"
+          @click.stop="saveCurrentMap" 
+        >
+          <i class="material-icons mr-1">done</i>
+          Save
+        </a>
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-primary text-white mr_1"
+          @click.stop="$refs.openMapModal.close" 
+        >
+          <i class="material-icons mr-1">cancel</i>
+          Cancel
+        </a>
+      </div>
+    </sweet-modal>
+    <sweet-modal ref="resetMapModal" class="of_v">
+      <div class="sweet_model_icon_div">
+        <div class="radius_circle bg-warning center_flex mlr_a">
+          <i class="material-icons text-white">restore</i>
+        </div>
+      </div>
+      <h3 class="f_smooth_auto">Are you Sure you want to reset this Map?</h3>
+      <p class="text-muted fs_18">Your current changes will be discarded, you won't be able to revert this!</p>
+      <div class="center_flex mt_2">
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-danger text-white mr_1"
+          @click.stop="resetMindmapap" 
+        >
+          <i class="material-icons mr-1">done</i>
+          Continue
+        </a>
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-primary text-white mr_1"
+          @click.stop="$refs.resetMapModal.close" 
+        >
+          <i class="material-icons mr-1">cancel</i>
+          Cancel
+        </a>
+      </div>
+    </sweet-modal>
   </div>
 </template>
 
 <script>
   import InputField from './idea_input_field'
   import http from '../../common/http'
+  import {SweetModal }from 'sweet-modal-vue';
 
   export default {
-    components: {InputField},
+    components: {InputField, SweetModal},
     data() {
       return {
         mainIdea: "Central Idea",
@@ -37,7 +166,21 @@
         y: 0,
         parentX: 0,
         parentY: 0,
-        windowWidth: window.innerWidth
+        windowWidth: window.innerWidth,
+        windowHeight: window.innerHeight,
+        saveMapName: '',
+        openMapmind: {
+          name: '',
+          id: ''
+        }
+      }
+    },
+    computed: {
+      C_centeralNodePosition() {
+        return {
+          left: (Math.floor(this.windowWidth/2) - 100) +'px', 
+          top: (Math.floor(this.windowHeight/2) - 50) +'px'
+        }
       }
     },
     methods: {
@@ -71,7 +214,7 @@
           c.style.top = 0;
           c.style.left = 0;
           c.width = this.windowWidth;
-          c.height = 1500;
+          c.height = this.windowHeight;
         }
         this.x = this.y = 0;
       },
@@ -115,6 +258,22 @@
       },
       getNodeStyle(node) {
         return {left: node.left +'px', top: node.top +'px'}
+      },
+      openNewMap() {
+        this.getNewMindmap();
+        this.$refs.newMapModal.close()
+      },
+      openNewMapNewWindow() {
+        window.open(window.location.origin+'/mindmaps/new')
+      },
+      saveCurrentMap() {
+        this.$refs.saveMapModal.close();
+      },
+      resetMindmapap() {
+        // Add logic to reset same map on frontend and backend both
+        // Temporaryly adding new map instead of resetting 
+        this.getNewMindmap();
+        this.$refs.resetMapModal.close();
       }
     },
     mounted() {
@@ -189,8 +348,8 @@
     border-radius: 50%;
     display: inline-block;
     position: absolute;
-    left: 130px;
-    top: 40px;
+    left: 120px;
+    top: 30px;
   }
   .start_dot:hover {
     border: 5px solid cornflowerblue;
